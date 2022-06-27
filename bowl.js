0e06fee81b286c0f409ed9ce4dd2f443c60c4a80
zeus <zeusofolympus1000@gmail.com>
2022-06-27 08:33:49 -0400

    create temp id files for local pipa to send to from bowlpath, then use stream to socket every few seconds

--- bowl.js
+++ bowl.js
@@ -8,104 +8,36 @@ export class BowlClient {
   constructor(host, port, bowlname){
     this.bowlname = bowlname
     this.bowlpath = './tmp/'+this.bowlname
-    this.mkdr()
-    this.socket = net.createConnection({host:host, port:port})
-    this.config_socket()
-    this.rstream = fs.createReadStream(this.bowlpath)
-    this.config_read_stream()
-    this.file_watcher = fs.watch(this.bowlpath, {persistent:true})
-    this.config_fsWatcher()
+    this.mkdr(this.bowlpath)
+    this.sock = new Socket({allowHalfOpen:true, writable:true})
+    this.sock.connect({port:3000, host:'localhost', keepAlive:true})
+    setInterval(()=>{
+      //create a new file for local pipa to pipe to
+      //create local pipa object, and pipa the data to the tmp_uniqueId file from bowlpath
+      //then use stream(tmp_uniqueId). local pipa cleans after itself in bowlpath every iteration.
+
+    }, 2500)
+    
   }
 
-  config_fsWatcher(){
-    this.file_watcher.on('close',()=>{
-      var data = this.rstream.read()
-      if (data !== null){
-        this.socket.write(data, ()=>{})
+  stream(source){
+    pipeline(
+      fs.createReadStream(source),
+      this.sock,
+      (err)=>{
+          if (err){
+              console.log('Pipeline failed', err)
+          }else{
+              console.log('Pipeline succeeded');
+          }
       }
-      this.file_watcher = fs.watch(this.bowlpath, {persistent:true})
-      this.config_fsWatcher()
-
-    });
-    this.file_watcher.on('change', ()=>{
-      var data = this.rstream.read()
-      if (data !== null){
-        this.socket.write(data, ()=>{})
-      }
-    });
-    this.file_watcher.on('error', (eventType, filename)=>{
-      var data = this.rstream.read()
-      if (data !== null){
-        this.socket.write(data, ()=>{})
-      }
-      this.file_watcher = fs.watch(this.bowlpath, {persistent:true})
-      this.config_fsWatcher()
-    });
+    );
   }
-  config_read_stream(){
-    this.rstream.on('close',()=>{
-      console.log('read stream has closed')
-
-      var data = this.rstream.read()
-      if (data !== null){
-        this.socket.write(data+'\n')
+  mkdr(path){
+      if (!fs.existsSync(path.dirname(path))){
+        fs.mkdirSync(path.dirname(path));
       }
-      this.rstream = fs.createReadStream(this.bowlpath)
-      this.config_read_stream()
-    })
-    this.rstream.on('open', ()=>{
-      console.log('read stream has opened')
-      var data = this.rstream.read()
-      if (data !== null){
-        this.socket.write(data)
-      }
-    })
-    this.rstream.on('ready', ()=>{
-      console.log('read stream is ready')
-      var data = this.rstream.read()
-      if (data !== null){
-        this.socket.write(data)
-      }
-
-    })
-
-  }
-  config_socket(){
-    this.socket.allowHalfOpen=true
-    this.socket.on('close', (error)=>{
-      console.log('socket closed: ', error)
-    });
-    this.socket.on('connect', (conn)=>{
-      console.log('connection established:', conn)
-    })
-    this.socket.on('drain', ()=>{
-      this.socket.resume()
-    })
-    this.socket.on('end', ()=>{
-      this.socket.resume()
-    })
-    this.socket.on('error', (err)=>{
-      console.log('Socket error:', err)
-    })
-    this.socket.on('timeout', ()=>{
-      this.socket.resume()
-    })
-    this.socket.on('lookup', ()=>{
-      console.log('looking up host')
-    })
-    this.socket.on('data', (data)=>{
-      this.socket.write(data)
-    })
-
-
-  }
-
-  mkdr(){
-      if (!fs.existsSync(path.dirname(this.bowlpath))){
-        fs.mkdirSync(path.dirname(this.bowlpath));
-      }
-      console.log("grabbing a bowl", this.bowlpath)
-      fs.writeFileSync(this.bowlpath, "", {flag:'a'})
+      fs.writeFileSync(path, "", {flag:'a'})
   }
 
   
---
+++ misc/experimentalClient.js
@@ -0,0 +1,8 @@
+import * as fs from "node:fs";
+import { Socket } from "node:net";
+import { pipeline } from "node:stream";
+import * as path from "node:path";
+
+
+
+
