4515c4b6a8c8429276173882b5aff971926ec652
zeus <zeusofolympus1000@gmail.com>
2022-06-27 07:24:40 -0400

    telnet can do a chat session witht the pipa server without a problem

--- misc/localPipaToSocket.js
+++ misc/localPipaToSocket.js
@@ -1,12 +1,14 @@
 import * as fs from "node:fs";
+import * as net from 'node:net';
 import * as path from "node:path";
-
 class LocalPipa{
 constructor(f1, f2){
         this.mkdir(f1)
         this.mkdir(f2)
         this.f1 = f1
         this.f2 = f2
+        this.socket = new net.Socket({allowHalfOpen:true, writable:true, readable:true})
+        
         this.rstream = fs.createReadStream(this.f1, {flags:'r+', pos:0})
         this.wstream = fs.createWriteStream(this.f2, {flags:'a', start:0})
         this.pstream = this.rstream.pipe(this.wstream, {end:false}, ()=>{})  
@@ -33,9 +35,7 @@ constructor(f1, f2){
             fs.mkdirSync(path.dirname(f));
           }
           fs.writeFileSync(f,"", {flag:'a'})
-
     }
 }
 //This would be used to keep the user source file clean and allow us to take our time processing the data
 new LocalPipa('./tmp/local.bowl', './tmp/local.filter')
-//107,374,182,400 bytes
