ab5eb950aa105492213526c2735a409a824d803b
zeus <zeusofolympus1000@gmail.com>
2022-06-27 20:04:37 -0400

    need to retest

--- localPipa.js
+++ localPipa.js
@@ -2,40 +2,34 @@ import * as fs from "node:fs";
 import * as path from "node:path";
 
 export class LocalPipa{
-    constructor(f1, f2, callback, iterMode=true){
-        this.iterMode=iterMode
-        this.mkdir(f1)
-        this.mkdir(f2)
-        this.f1 = f1
-        this.f2 = f2
-        this.rstream = fs.createReadStream(this.f1, {flags:'r+', pos:0})
-        this.wstream = fs.createWriteStream(this.f2, {flags:'a', start:0})
-        this.pstream = this.rstream.pipe(this.wstream, {end:false}, ()=>{})  
-        this.counter = 0;
-        this.wstream.on('data', (src)=>{
-            this.wstream.start+=1
-            this.rstream.pos+=1
-            if ((this.counter>=10000)&&(this.iterMode)){
-                this.wstream.close()
-            }else{
-                this.counter+=1
-            }
 
-        })
-        this.rstream.on('close', ()=>{
+    //This can be batch jobbed if you make promisory
+    //and it can do a single job with a single read/write stream pipe
+    pipa(f1, f2, callback){
+        this.mkdir(f1);
+        this.mkdir(f2);
+        var counter = 0;
+        var rstream = fs.createReadStream(f1, {flags:'r+'});
+        var wstream = fs.createWriteStream(f2, {flags:'a'});
+        rstream.on('close', ()=>{
             //clean up read file
-            fs.open(this.f1, 'w', (err, fd) => {
-                for (var i = 0; i<=this.counter; i+=1){
-                    fs.write(fd, "",0, {}, ()=>{
-                    })
+            console.log(rstream.bytesRead, "bytes read through stream")
+            console.log("cleaning ", rstream.bytesRead, " bytes from ", f1)
+            //use these values if you find a better way to erase
+            // var f1Size = fs.stat(f1).size
+            // var f1NewSize = f1Size-rstream.bytesRead
+            //do not use w flag as it truncates the file
+            fs.open(f1, 'r+', (err, fd) => {
+                for (var i = 0; i<=rstream.bytesRead; i+=1){
+                    fs.write(fd, "",0, {}, ()=>{});
                 }
-                fs.close(fd)
-                this.counter=0
-                
+                fs.close(fd);
             });
-            callback()
-        })
+            callback();
+        });
+        rstream.pipe(wstream, {end:false}, ()=>{})  
     }
+
     mkdir(f){
         if (!fs.existsSync(path.dirname(f))){
             fs.mkdirSync(path.dirname(f));
@@ -45,5 +39,5 @@ export class LocalPipa{
     }
 }
 //This would be used to keep the user source file clean and allow us to take our time processing the data
-// new LocalPipa('./tmp/local.bowl', './tmp/local.filter')
-//107,374,182,400 bytes
+var lp = new LocalPipa()
+lp.pipa('./tmp/new.bowl', './tmp/new.filter', ()=>{})
