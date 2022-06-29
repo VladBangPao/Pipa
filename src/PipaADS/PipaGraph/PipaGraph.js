041bd3c07b2b381e717bcf2d7710b9dee9751c29
Hassan Amad <hassan@Hassans-MacBook-Pro.local>
2022-06-28 22:53:01 -0400

    abstract data structures

---
+++ src/PipaADS/PipaGraph/PipaGraph.js
@@ -0,0 +1,27 @@
+import { PipaObject } from "../../PipaObject/PipaObject";
+
+class PipaGraph extends PipaObject{
+    constructor(max_width, max_height){
+        this.size=0;
+    }
+
+    depth_first_search(){
+
+    }
+    width_first_search(){
+        
+    }
+    //adding to an abstract graph is simple, but not as useful as a specific type
+    //this should only be used for arbitrary clustering
+    add(){
+
+    }
+    remove(options={}){
+
+    }
+    get_size(){
+
+    }
+
+
+}
